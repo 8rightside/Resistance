9d04cd559f4b2d1b5fe66e8508ea3953dc8a2b94
malenko <mattmalenko17@gmail.com>
2021-01-08 19:44:20 +0000

    Added some more tests to ResistorControllerTests

--- Tests/ResistanceTests/ResistorControllerTests.swift
+++ Tests/ResistanceTests/ResistorControllerTests.swift
@@ -1,4 +1,8 @@
+// MARK: Resistor creation
 
+
+
+// MARK: Valid Input
 extension ResistorControllerTests {
     func testCreateFourBandOrFail_withValidInput() throws {
         var result: Resistor
@@ -19,25 +23,77 @@ extension ResistorControllerTests {
         XCTAssertEqual(result.value, 99_000_000_000)
     }
     
+    func testCreateFiveBandOrFail_withValidInput() throws {
+        var result: Resistor
+        
+        result = try sut.createFiveBandOrFail(from: 0.01)
+        XCTAssertEqual(result.value, 0.01)
+        
+        result = try sut.createFiveBandOrFail(from: 23)
+        XCTAssertEqual(result.value, 23)
+        
+        result = try sut.createFiveBandOrFail(from: 545)
+        XCTAssertEqual(result.value, 545)
+        
+        result = try sut.createFiveBandOrFail(from: 37_500)
+        XCTAssertEqual(result.value, 37_500)
+        
+        result = try sut.createFiveBandOrFail(from: 999_000_000_000)
+        XCTAssertEqual(result.value, 999_000_000_000)
+    }
+    
+    func testCreateSixBandOrFail_withValidInput() throws {
+        var result: Resistor
+        
+        result = try sut.createFiveBandOrFail(from: 0.01)
+        XCTAssertEqual(result.value, 0.01)
+        
+        result = try sut.createFiveBandOrFail(from: 23)
+        XCTAssertEqual(result.value, 23)
+        
+        result = try sut.createFiveBandOrFail(from: 545)
+        XCTAssertEqual(result.value, 545)
+        
+        result = try sut.createFiveBandOrFail(from: 37_500)
+        XCTAssertEqual(result.value, 37_500)
+        
+        result = try sut.createFiveBandOrFail(from: 999_000_000_000)
+        XCTAssertEqual(result.value, 999_000_000_000)    }
+}
+
+// MARK: Invalid Input
+extension ResistorControllerTests {
     func testCreateFourBandOrFail_withInvalidInput() {
+        let lowValue = 0.001
+        XCTAssertThrowsError(try sut.createFourBandOrFail(from: lowValue))
         
-    }
-    
-    func testCreateFiveBandOrFail_withValidInput() {
+        let highValue = 100_000_000_000.0
+        XCTAssertThrowsError(try sut.createFourBandOrFail(from: highValue))
         
+        let invalidValue = 234.0
+        XCTAssertThrowsError(try sut.createFourBandOrFail(from: invalidValue))
     }
-    
+        
     func testCreateFiveBandOrFail_withInvalidInput() {
+        let lowValue = 0.001
+        XCTAssertThrowsError(try sut.createFourBandOrFail(from: lowValue))
         
-    }
-    
-    func testCreateSixBandOrFail_withValidInput() {
+        let highValue = 999_000_000_001.0
+        XCTAssertThrowsError(try sut.createFourBandOrFail(from: highValue))
         
+        let invalidValue = 2341.0
+        XCTAssertThrowsError(try sut.createFourBandOrFail(from: invalidValue))
     }
     
     func testCreateSixBandOrFail_withInvalidInput() {
+        let lowValue = 0.001
+        XCTAssertThrowsError(try sut.createFourBandOrFail(from: lowValue))
         
-    }
+        let highValue = 999_000_000_001.0
+        XCTAssertThrowsError(try sut.createFourBandOrFail(from: highValue))
+        
+        let invalidValue = 2341.0
+        XCTAssertThrowsError(try sut.createFourBandOrFail(from: invalidValue))    }
 }
 
 // MARK:- Internal
@@ -57,10 +113,11 @@ final class ResistorControllerTests: XCTestCase {
     
     static var allTests = [
         ("testCreateFourBandOrFail_withValidInput", testCreateFourBandOrFail_withValidInput),
-        ("testCreateFourBandOrFail_withInvalidInput", testCreateFourBandOrFail_withInvalidInput),
         ("testCreateFiveBandOrFail_withValidInput", testCreateFiveBandOrFail_withValidInput),
-        ("testCreateFiveBandOrFail_withInvalidInput", testCreateFiveBandOrFail_withInvalidInput),
         ("testCreateSixBandOrFail_withValidInput", testCreateSixBandOrFail_withValidInput),
+        
+        ("testCreateFourBandOrFail_withInvalidInput", testCreateFourBandOrFail_withInvalidInput),
+        ("testCreateFiveBandOrFail_withInvalidInput", testCreateFiveBandOrFail_withInvalidInput),
         ("testCreateSixBandOrFail_withInvalidInput", testCreateSixBandOrFail_withInvalidInput),
     ]
 }
