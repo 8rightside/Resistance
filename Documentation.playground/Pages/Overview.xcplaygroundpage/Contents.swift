5a35de439f4b763a96769575aef441a7f2b3de51
malenko <mattmalenko17@gmail.com>
2021-02-07 22:40:18 +0000

    edited docs

--- Documentation.playground/Pages/Overview.xcplaygroundpage/Contents.swift
+++ Documentation.playground/Pages/Overview.xcplaygroundpage/Contents.swift
@@ -1,11 +1,13 @@
 /*:
- ![Resistance](logo.png)
-  This is `Resistance`, a Swift library for creating and manipulating resistors and their values. Use the following links to navigate the docs.
- 
  ## Overview
  * [The Resistor type](Resistor%20Type)
  * [Resistor Properties](Resistor%20Properties)
  * [Tolerance and TempCo Ranges](Tolerance%20and%20TempCo%20Ranges)
  * [Using ResistorFactory](Using%20ResistorFactory)
  * [Using ESeries](Using%20ESeries)
+ 
+ ![Resistance](logo.png)
+ 
+ 
+ 
  */
